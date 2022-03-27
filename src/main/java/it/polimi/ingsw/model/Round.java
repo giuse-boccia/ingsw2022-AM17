83926cfb9776d501820ab361e93e75f1e42b772d
Giuseppe Boccia <giuseppe.boccia@mail.polimi.it>
2022-03-27 18:01:24 +0200

    Initial implementation of Round

--- src/main/java/it/polimi/ingsw/model/Round.java
+++ src/main/java/it/polimi/ingsw/model/Round.java
@@ -3,10 +3,23 @@ package it.polimi.ingsw.model;
 import java.util.ArrayList;
 
 public class Round {
+    private final int firstPlayerIndex;
+    private PlanningPhase planningPhase;
+    private PlayerActionPhase currentPlayerActionPhase;
 
-    PlanningPhase planningPhase;
-    PlayerActionPhase[] actionPhases;
-    int firstPlayerIndex;
+    public Round(int firstPlayerIndex) {
+        this.firstPlayerIndex = firstPlayerIndex;
+    }
 
+    public PlanningPhase getPlanningPhase() {
+        return planningPhase;
+    }
 
+    public PlayerActionPhase getCurrentPlayerActionPhase() {
+        return currentPlayerActionPhase;
+    }
+
+    public int getFirstPlayerIndex() {
+        return firstPlayerIndex;
+    }
 }
