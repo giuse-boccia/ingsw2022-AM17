1e6f4fc523c5b564971ebeca817fbc9e92d94631
claudioarione <claudio.arione@mail.polimi.it>
2022-03-30 13:12:23 +0200

    Added StudentNotOnTheCardException

---
+++ src/main/java/it/polimi/ingsw/exceptions/StudentNotOnTheCardException.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.exceptions;
+
+public class StudentNotOnTheCardException extends Exception {
+
+    public StudentNotOnTheCardException(String message) {
+        super(message);
+    }
+}
