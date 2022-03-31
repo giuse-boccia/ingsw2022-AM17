b184aa585ba8f5c63ecc1932581df5613613ad68
Giuseppe Boccia <giuseppe.boccia@mail.polimi.it>
2022-03-31 22:39:02 +0200

    Added main manifest attribute in pom.xml

--- src/main/java/it/polimi/ingsw/App.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw;
-
-public class App
-{
-    public static void main( String[] args ) {
-        // Create Game and call setup() and start()
-    }
-}
---
+++ src/main/java/it/polimi/ingsw/Main.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test print to check if the jar works
+        System.out.println("Hello World!");
+    }
+}
