856b99ccd49b555e82c57ec0edb8d1a80acbaf7a
claudioarione <claudio.arione@mail.polimi.it>
2022-05-09 19:30:04 +0200

    Added fields in ActionArgs class

--- src/main/java/it/polimi/ingsw/messages/action/ActionArgs.java
+++ src/main/java/it/polimi/ingsw/messages/action/ActionArgs.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.messages.action;
 
+import it.polimi.ingsw.model.characters.CharacterName;
 import it.polimi.ingsw.model.game_objects.Color;
+
+import java.util.List;
 
 public class ActionArgs {
 
@@ -9,6 +12,9 @@ public class ActionArgs {
     private Integer num_steps;
     private Integer cloud;
     private Integer value;
+    private CharacterName characterName;
+    private List<Color> sourceStudents;
+    private List<Color> dstStudents;
 
     public ActionArgs() {
     }
@@ -53,4 +59,27 @@ public class ActionArgs {
         this.value = value;
     }
 
+    public CharacterName getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(CharacterName characterName) {
+        this.characterName = characterName;
+    }
+
+    public List<Color> getSourceStudents() {
+        return sourceStudents;
+    }
+
+    public void setSourceStudents(List<Color> sourceStudents) {
+        this.sourceStudents = sourceStudents;
+    }
+
+    public List<Color> getDstStudents() {
+        return dstStudents;
+    }
+
+    public void setDstStudents(List<Color> dstStudents) {
+        this.dstStudents = dstStudents;
+    }
 }
