c37fbc84f81b946c89cc73d6b055277e7faa3135
Giuseppe Boccia <giuseppe.boccia@mail.polimi.it>
2022-05-31 14:54:59 +0200

    Added fields (and setters) to GameLobby

--- src/main/java/it/polimi/ingsw/messages/login/GameLobby.java
+++ src/main/java/it/polimi/ingsw/messages/login/GameLobby.java
@@ -2,13 +2,27 @@ package it.polimi.ingsw.messages.login;
 
 public class GameLobby {
     private String[] players;
-    private final int numPlayers;
-    private final boolean isExpert;
+    private int numPlayers;
+    private boolean isExpert;
+    private boolean isSaved;
+    private String[] allPlayers;
 
+    // new game
     public GameLobby(String[] players, int numPlayers, boolean isExpert) {
         this.players = players;
         this.numPlayers = numPlayers;
         this.isExpert = isExpert;
+        this.isSaved = false;
+        this.allPlayers = null;
+    }
+
+    // loading from save
+    public GameLobby(String[] players, int numPlayers, boolean isExpert, boolean isSaved, String[] allPlayers) {
+        this.players = players;
+        this.numPlayers = numPlayers;
+        this.isExpert = isExpert;
+        this.isSaved = isSaved;
+        this.allPlayers = allPlayers;
     }
 
     public String[] getPlayers() {
@@ -23,7 +37,31 @@ public class GameLobby {
         return numPlayers;
     }
 
+    public void setNumPlayers(int numPlayers) {
+        this.numPlayers = numPlayers;
+    }
+
     public boolean isExpert() {
         return isExpert;
+    }
+
+    public void setExpert(boolean expert) {
+        isExpert = expert;
+    }
+
+    public boolean isSaved() {
+        return isSaved;
+    }
+
+    public void setSaved(boolean saved) {
+        isSaved = saved;
+    }
+
+    public String[] getAllPlayers() {
+        return allPlayers;
+    }
+
+    public void setAllPlayers(String[] allPlayers) {
+        this.allPlayers = allPlayers;
     }
 }
