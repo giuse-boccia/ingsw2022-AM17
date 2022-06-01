a49ef4754a8c649e80f4615f3db87d291bd579ad
claudioarione <claudio.arione@mail.polimi.it>
2022-06-01 18:31:03 +0200

    NetworkClient has a method that initializes all the observers needed for the game

--- src/main/java/it/polimi/ingsw/client/NetworkClient.java
+++ src/main/java/it/polimi/ingsw/client/NetworkClient.java
@@ -1,15 +1,21 @@
 package it.polimi.ingsw.client;
 
-import it.polimi.ingsw.messages.login.ClientLoginMessage;
-import it.polimi.ingsw.model.characters.CharacterName;
-import it.polimi.ingsw.model.game_objects.Color;
+import it.polimi.ingsw.client.observers.choices.action.SendActionChoiceObserver;
+import it.polimi.ingsw.client.observers.choices.character.SendCharacterChoiceObserver;
+import it.polimi.ingsw.client.observers.game_actions.choose_cloud.SendChooseCloudObserver;
+import it.polimi.ingsw.client.observers.game_actions.move_mn.SendMoveMNObserver;
+import it.polimi.ingsw.client.observers.game_actions.move_student.SendMoveStudentObserver;
+import it.polimi.ingsw.client.observers.game_actions.play_assistant.SendPlayAssistantObserver;
+import it.polimi.ingsw.client.observers.game_actions.play_character.SendPlayCharacterObserver;
+import it.polimi.ingsw.client.observers.login.game_parameters.SendGameParametersObserver;
+import it.polimi.ingsw.client.observers.login.load_game.ExecuteLoadGameObserver;
+import it.polimi.ingsw.client.observers.login.username.SendUsernameObserver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.List;
 
 public class NetworkClient extends Thread {
     private final Client client;
@@ -55,9 +61,28 @@ public class NetworkClient extends Thread {
         socketOut.println(message);
     }
 
+    private void attachObserversToMessageHandler(MessageHandler mh) {
+        // Login observers
+        new SendUsernameObserver(mh);
+        new SendGameParametersObserver(mh);
+        new ExecuteLoadGameObserver(mh);
+
+        // Choice observers
+        new SendActionChoiceObserver(mh);
+        new SendCharacterChoiceObserver(mh);
+
+        // Game actions observers
+        new SendChooseCloudObserver(mh);
+        new SendMoveMNObserver(mh);
+        new SendMoveStudentObserver(mh);
+        new SendPlayAssistantObserver(mh);
+        new SendPlayCharacterObserver(mh);
+    }
+
     @Override
     public void run() {
         MessageHandler mh = new MessageHandler(NetworkClient.this);
+        attachObserversToMessageHandler(mh);
 
         try {
             mh.askUsernameAndSend();
