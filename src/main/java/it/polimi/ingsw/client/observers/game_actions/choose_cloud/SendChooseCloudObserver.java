f5852924175455b6051ea82349af02116da01256
claudioarione <claudio.arione@mail.polimi.it>
2022-06-01 18:18:55 +0200

    Added observer and relative concrete class for the choice of a cloud

---
+++ src/main/java/it/polimi/ingsw/client/observers/game_actions/choose_cloud/SendChooseCloudObserver.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.client.observers.game_actions.choose_cloud;
+
+import it.polimi.ingsw.client.MessageHandler;
+import it.polimi.ingsw.constants.Messages;
+import it.polimi.ingsw.messages.action.Action;
+import it.polimi.ingsw.messages.action.ActionArgs;
+import it.polimi.ingsw.messages.action.ClientActionMessage;
+
+public class SendChooseCloudObserver implements ChooseCloudObserver {
+
+    private final MessageHandler mh;
+
+    public SendChooseCloudObserver(MessageHandler mh) {
+        this.mh = mh;
+        mh.attachChooseCloudObserver(this);
+    }
+
+    @Override
+    public void onCloudChosen(int index) {
+        ActionArgs args = new ActionArgs();
+        args.setCloud(index);
+
+        Action action = new Action(Messages.ACTION_FILL_FROM_CLOUD, args);
+
+        ClientActionMessage toSend = new ClientActionMessage();
+        toSend.setAction(action);
+        toSend.setPlayer(mh.getNetworkClient().getClient().getUsername());
+
+        mh.getNetworkClient().sendMessageToServer(toSend.toJson());
+    }
+}
