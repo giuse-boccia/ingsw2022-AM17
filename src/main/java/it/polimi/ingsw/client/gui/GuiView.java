d8f1870d9968d8794f338ef5f1f54a7739a23fa6
Void <riccardo.begliomini@mail.polimi.it>
2022-06-01 19:31:45 +0200

    Added Cranio symbol in window bar of GUI

--- src/main/java/it/polimi/ingsw/client/gui/GuiView.java
+++ src/main/java/it/polimi/ingsw/client/gui/GuiView.java
@@ -19,6 +19,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
@@ -197,6 +198,7 @@ public class GuiView extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         GuiView.stage = stage;
+        stage.getIcons().add(new Image("/gameboard/cranio_logo.png"));
         currentSceneName = "login";
         FXMLLoader fxmlLoader = new FXMLLoader(GuiView.class.getResource("/login.fxml"));
 
