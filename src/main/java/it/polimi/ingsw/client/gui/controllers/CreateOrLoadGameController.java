7c4ba5f4c7c43dac29893994109dd145e2a41c1b
claudioarione <claudio.arione@mail.polimi.it>
2022-06-14 11:26:47 +0200

    Minor fixes to create or load scene

--- src/main/java/it/polimi/ingsw/client/gui/controllers/CreateOrLoadGameController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/CreateOrLoadGameController.java
@@ -6,23 +6,27 @@ import it.polimi.ingsw.messages.login.GameLobby;
 import it.polimi.ingsw.model.characters.CharacterName;
 import it.polimi.ingsw.server.game_state.GameState;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 import java.util.List;
 
 public class CreateOrLoadGameController implements GuiController {
 
-    @FXML
-    private Button createGameBtn;
-    @FXML
-    private Button loadGame;
-
+    /**
+     * This function is called when the user clicks on the Load Game button
+     *
+     * @param event the user's click on the load game button
+     */
     @FXML
     void onLoadGameButtonClicked(MouseEvent event) {
         GuiView.getGui().getCurrentObserverHandler().notifyAllLoadGameObservers();
     }
 
+    /**
+     * This function is called when the user clicks on the New Game button
+     *
+     * @param event the user's click on the new game button
+     */
     @FXML
     void onNewGameButtonClicked(MouseEvent event) {
         GuiView.getGui().askNumPlayersAndExpertMode();
