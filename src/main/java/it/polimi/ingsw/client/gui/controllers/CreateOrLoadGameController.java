3b6042416f31fa6f4e182ed9615e1b50c8baf86f
claudioarione <claudio.arione@mail.polimi.it>
2022-06-23 12:55:47 +0200

    Remove hardcoded message in create_or_load.fxml

--- src/main/java/it/polimi/ingsw/client/gui/controllers/CreateOrLoadGameController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/CreateOrLoadGameController.java
@@ -2,15 +2,32 @@ package it.polimi.ingsw.client.gui.controllers;
 
 import it.polimi.ingsw.client.gui.GuiView;
 import it.polimi.ingsw.client.gui.utils.GuiCharacterType;
+import it.polimi.ingsw.languages.MessageResourceBundle;
 import it.polimi.ingsw.messages.login.GameLobby;
 import it.polimi.ingsw.model.characters.CharacterName;
 import it.polimi.ingsw.server.game_state.GameState;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 import java.util.List;
 
 public class CreateOrLoadGameController implements GuiController {
+
+    @FXML
+    private Text askCreateOrLoadTitle;
+    @FXML
+    private Button loadGameBtn;
+    @FXML
+    private Button newGameBtn;
+
+    @FXML
+    private void initialize() {
+        askCreateOrLoadTitle.setText(MessageResourceBundle.getMessage("ask_create_or_load"));
+        newGameBtn.setText(MessageResourceBundle.getMessage("new_game"));
+        loadGameBtn.setText(MessageResourceBundle.getMessage("load_game"));
+    }
 
     /**
      * This function is called when the user clicks on the Load Game button
