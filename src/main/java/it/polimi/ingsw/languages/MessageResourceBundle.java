2623b0dd05339a4205949d77ee8d240109d7626b
claudioarione <claudio.arione@mail.polimi.it>
2022-06-24 20:05:36 +0200

    Add JavaDoc to MessageResourceBundle methods

--- src/main/java/it/polimi/ingsw/languages/MessageResourceBundle.java
+++ src/main/java/it/polimi/ingsw/languages/MessageResourceBundle.java
@@ -7,11 +7,12 @@ public class MessageResourceBundle {
 
     private static ResourceBundle messages;
 
-    private static void setLanguage(String tag) {
-        Locale currentLocale = new Locale.Builder().setLanguageTag(tag).build();
-        messages = ResourceBundle.getBundle("messages/messages", currentLocale);
-    }
-
+    /**
+     * Returns the message corresponding to the input key
+     *
+     * @param key the key to find the corresponding value of
+     * @return the message corresponding to the input key
+     */
     public static String getMessage(String key) {
         // Send the message in the selected language, if present
         if (messages.containsKey(key)) {
@@ -22,11 +23,17 @@ public class MessageResourceBundle {
         return englishMessages.getString(key);
     }
 
+    /**
+     * Sets up the {@code ResourceBundle} according to the given language tag
+     *
+     * @param tag the {@code String} representing the code of the desired language
+     */
     public static void initializeBundle(String tag) {
         if (tag == null || (!tag.equals("en") && !tag.equals("it"))) {
             tag = "en";
         }
-        setLanguage(tag);
+        Locale currentLocale = new Locale.Builder().setLanguageTag(tag).build();
+        messages = ResourceBundle.getBundle("messages/messages", currentLocale);
     }
 
 }
