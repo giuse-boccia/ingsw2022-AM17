5fa026d2bba7916f4b3471901998d6814256cced
claudioarione <claudio.arione@mail.polimi.it>
2022-06-30 21:05:04 +0200

    Add Javadoc to ChatObserver

--- src/main/java/it/polimi/ingsw/client/observers/chat/ChatObserver.java
+++ src/main/java/it/polimi/ingsw/client/observers/chat/ChatObserver.java
@@ -4,6 +4,11 @@ import it.polimi.ingsw.messages.chat.ChatMessage;
 
 public interface ChatObserver {
 
+    /**
+     * This method is triggered when a player clicks on Send button from chat screen
+     *
+     * @param message the sent {@link ChatMessage}
+     */
     void onMessageSent(ChatMessage message);
 
 }
