c7795dcc3c62967651af127f303a39adcc3a8f7a
claudioarione <claudio.arione@mail.polimi.it>
2022-06-30 21:07:21 +0200

    Add utility class for the chat

---
+++ src/main/java/it/polimi/ingsw/client/gui/utils/ChatView.java
@@ -0,0 +1,51 @@
+package it.polimi.ingsw.client.gui.utils;
+
+import it.polimi.ingsw.client.gui.GuiView;
+import it.polimi.ingsw.client.gui.controllers.ChatController;
+import it.polimi.ingsw.languages.MessageResourceBundle;
+import it.polimi.ingsw.messages.chat.ChatMessage;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChatView {
+
+    private static boolean isChatAlreadyOpened = false;
+    private static ChatController chatController;
+
+    /**
+     * Shows the chat panel, if not already open
+     */
+    public static void openChatPanel() {
+        if (isChatAlreadyOpened) return;
+        isChatAlreadyOpened = true;
+        FXMLLoader loader = new FXMLLoader(GuiView.class.getResource("/chat.fxml"));
+        try {
+            Scene scene = new Scene(loader.load(), 400, 400);
+            chatController = loader.getController();
+            Stage stage = new Stage();
+            stage.setTitle("Chat");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setOnCloseRequest(event -> isChatAlreadyOpened = false);
+            stage.show();
+
+        } catch (IOException e) {
+            GuiView.getGui().gracefulTermination(MessageResourceBundle.getMessage("generic_error_gui"));
+        }
+    }
+
+    /**
+     * Sends the received chat message to the correct controller
+     *
+     * @param message the received {@link ChatMessage}
+     */
+    public static void onChatMessageReceived(ChatMessage message) {
+        if (!isChatAlreadyOpened) return;
+        Platform.runLater(() -> chatController.onChatMessageReceived(message));
+    }
+
+}
