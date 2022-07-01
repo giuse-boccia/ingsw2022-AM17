3f7aad9ce7257e77e502a53bade3faa841923edb
claudioarione <claudio.arione@mail.polimi.it>
2022-07-01 04:53:52 +0200

    Minor refactor of class ActionController

--- src/main/java/it/polimi/ingsw/client/gui/controllers/ActionController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/ActionController.java
@@ -3,14 +3,21 @@ package it.polimi.ingsw.client.gui.controllers;
 import it.polimi.ingsw.client.gui.GuiView;
 import it.polimi.ingsw.client.gui.utils.ChatView;
 import it.polimi.ingsw.client.gui.utils.DrawingComponents;
+import it.polimi.ingsw.client.gui.utils.DrawingConstants;
 import it.polimi.ingsw.client.gui.utils.GuiCharacterType;
+import it.polimi.ingsw.languages.Messages;
 import it.polimi.ingsw.messages.login.GameLobby;
 import it.polimi.ingsw.model.characters.CharacterName;
 import it.polimi.ingsw.server.game_state.GameState;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 
 import java.util.List;
@@ -60,11 +67,30 @@ public class ActionController implements GuiController {
     private void drawGameState(GameState gameState, String username) {
         DrawingComponents.clearAll(root);
         DrawingComponents.drawComponents(gameState, width, height, root, username);
-        Circle openChatButton = new Circle(20, javafx.scene.paint.Color.WHITESMOKE);
-        openChatButton.setCenterX(width - 50);
-        openChatButton.setCenterY(height - 50);
+
+        root.getChildren().add(getOpenChatButton());
+    }
+
+    /**
+     * Returns a {@code VBox} containing a text and the button which has to be clicked in order to open the chat panel
+     *
+     * @return a {@code VBox} with a {@code Text} and a {@code Button}
+     */
+    private VBox getOpenChatButton() {
+        Button openChatButton = new Button("...");
+        openChatButton.setStyle("-fx-font-size: 24; -fx-background-color: white; -fx-background-radius: 40; -fx-border-radius: 40");
         openChatButton.setOnMouseClicked(event -> ChatView.openChatPanel());
-        root.getChildren().add(openChatButton);
+
+        Text chatText = new Text(Messages.getMessage("game_chat"));
+        chatText.setFont(Font.font(DrawingConstants.FONT_NAME, FontWeight.NORMAL, DrawingConstants.SUBTITLE_FONT_SIZE));
+
+        VBox chatVbox = new VBox(openChatButton, chatText);
+        chatVbox.setSpacing(10);
+        chatVbox.setAlignment(Pos.CENTER);
+        chatVbox.setLayoutX(width - 100);
+        chatVbox.setLayoutY(height - 120);
+
+        return chatVbox;
     }
 
     public void setRoot(AnchorPane root) {
